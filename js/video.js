68f2721db164ed62c28103ab2403f554b099ea59
Jack_yty <51208835+ytyi@users.noreply.github.com>
2022-04-13 16:21:36 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -16,13 +16,52 @@ document.querySelector("#pause").addEventListener('click', function(){
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
-	video.playbackRate =video.playbackRate* 0.95;
+	video.playbackRate *= 0.95;
 	console.log(`New speed is ${ video.playbackRate }`);
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	console.log("Speed Up");
-	video.playbackRate =video.playbackRate/ 0.95;
+
+	video.playbackRate /=0.95;
 	console.log(`New speed is ${ video.playbackRate }`);
 });
 
+document.querySelector("#skip").addEventListener("click", function() {
+	
+	video.currentTime += 15;
+	if (video.currentTime >= video.duration) {
+
+		video.currentTime = 0;
+	}
+	console.log(`Current location: ${video.currentTime}`);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == false) {
+
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+	else {
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	}	
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	
+	video.volume = this.value / 100
+
+	document.querySelector("#volume").innerHTML = `${video.volume * 100}%`
+});
+
+
+document.querySelector("#vintage").addEventListener("click", function() {
+
+	video.classList.add("oldSchool")
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+
+	video.classList.remove("oldSchool")
+});
